41340ed5e50f98b89e4fecb4efe66504778f5fe5
Tariku Kussia <119606542+TarikuGitHub@users.noreply.github.com>
2023-08-28 23:55:05 +0300

    we

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "http://127.0.0.1:5001/project-8040d/us-central1/api",
+  baseURL: `{process.env.REACT_APP_base_url}/project-8040d/us-central1/api`,
 });
 export default instance;

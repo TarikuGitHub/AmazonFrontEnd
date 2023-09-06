d9daaf0cbe7db5ea8867ff8f59bf85069032cc1b
Tariku Kussia <119606542+TarikuGitHub@users.noreply.github.com>
2023-09-06 10:55:34 +0300

    axios.js modified

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: `{process.env.REACT_APP_base_url}/project-8040d/us-central1/api`,
+  baseURL: "https://cloudy-puce-colt.cyclic.cloud",
+  // baseURL: "http://127.0.0.1:5001/project-8040d/us-central1/api",
+
+  // baseURL: `{process.env.REACT_APP_base_url}/project-8040d/us-central1/api`,
 });
 export default instance;
